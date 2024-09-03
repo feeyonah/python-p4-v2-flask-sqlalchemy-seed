4c45448f8ef173fb6e2e73100626a08eeb46a001
fiona <omburafiona@gmail.com>
2024-09-03 21:21:59 +0300

    seedng databases

--- server/seed.py
+++ server/seed.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-#server/seed.py
+# server/seed.py
+
+from random import choice as rc
+from faker import Faker
 
 from app import app
 from models import db, Pet
 
 with app.app_context():
 
-    # Create an empty list
+    fake = Faker()
+
+    # Clear out existing pets from the database
+    Pet.query.delete()
+
+    # Create an empty list to store pet objects
     pets = []
 
-    # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
 
-    # Insert each Pet in the list into the database table
+    for _ in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
+
+    # Add all pet objects to the session and commit them to the database
     db.session.add_all(pets)
-
-    # Commit the transaction
     db.session.commit()
